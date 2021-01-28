91923a5e34ce079508e81143c8e86cbce16529f6
Justin R. Zweifel <jzweifel15@yahoo.com>
2021-01-28 00:09:52 +0000

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,18 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {
+          movies.map(movie => 
+            <div>
+              <h3>{ movie.title }</h3>
+              <ul>
+                <li>Time: { movie.time }</li>
+                <li>Genres: { movie.genres }</li>
+              </ul>
+            </div>
+          )
+        }
     </div>
   );
 };
